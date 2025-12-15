341a7452ba37dfd4369d0561c310758c5a984c8e
onlyoneprivate <pllm098@naver.com>
2025-12-15 14:26:13 +0900

    ⚙️Chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/itzi/dearhaeny/DearHaenyApplication.java
@@ -0,0 +1,13 @@
+package com.itzi.dearhaeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearHaenyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DearHaenyApplication.class, args);
+    }
+
+}
