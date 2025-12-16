f5ab4c896736a7c39edf819110bdb5684d130a28
onlyoneprivate <pllm098@naver.com>
2025-12-16 13:35:29 +0900

    ✨Feat: 게시글 도메인 및 레포지토리 구현

---
+++ src/main/java/com/dearhaeny/dearhaeny/post/domain/PostType.java
@@ -0,0 +1,8 @@
+package com.dearhaeny.dearhaeny.post.domain;
+
+public enum PostType {
+
+    NEW_YEAR_WISH,                  // 새해 소원
+    INNER_THOUGHT,                  // 속마음
+    COURAGE                         // 용기 얻기
+}
