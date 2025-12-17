5885b4dfc1c60352bfaa7797e8addcc5bbf2598a
onlyoneprivate <pllm098@naver.com>
2025-12-17 17:09:18 +0900

    🐛Fix:중복 닉네임 응답 불일치 수정 (500 -> 409)

--- src/main/java/com/dearhaeny/dearhaeny/post/service/PostService.java
+++ src/main/java/com/dearhaeny/dearhaeny/post/service/PostService.java
@@ -23,7 +23,6 @@ public class PostService {
     public PostCreatedResponse sendPost(PostCreateRequest request) {
 
         // 게시글을 작성하기 위해서는 모든 필드가 작성돼 있어야 한다
-
         if (request.getNickname() == null || request.getNickname().isBlank()) {
             throw new GeneralException(ErrorStatus.VALIDATION_ERROR, "닉네임을 입력해 주세요.");
         }
@@ -40,22 +39,31 @@ public class PostService {
             throw new GeneralException(ErrorStatus.VALIDATION_ERROR, "게시글 타입을 선택해 주세요.");
         }
 
-        // Post 엔티티 생성
-        Post post = Post.builder()
-                .nickname(request.getNickname())
-                .postType(request.getPostType())
-                .content(request.getContent())
-                .build();
+        try {
+            // Post 엔티티 생성
+            Post post = Post.builder()
+                    .nickname(request.getNickname())
+                    .postType(request.getPostType())
+                    .content(request.getContent())
+                    .build();
 
-        // 저장
-        // 닉네임이 중복일 경우 DataIntegrityViolationException 발생
-        Post savedPost = postRepository.saveAndFlush(post);
+            // 저장
+            // 닉네임이 중복일 경우 DataIntegrityViolationException 발생
+            Post savedPost = postRepository.saveAndFlush(post);
 
-        // responseDto 반환
-        return PostCreatedResponse.builder()
-                .postId(savedPost.getPostId())
-                .postType(savedPost.getPostType())
-                .createdAt(post.getCreatedAt())
-                .build();
+            // responseDto 반환
+            return PostCreatedResponse.builder()
+                    .postId(savedPost.getPostId())
+                    .postType(savedPost.getPostType())
+                    .createdAt(post.getCreatedAt())
+                    .build();
+        } catch (Exception e) {
+            // 메시지에 중복이 포함되어 있다면 GeneralException으로 변환
+            if (e.getMessage().contains("Duplicate entry")) {
+                throw new GeneralException(ErrorStatus.DUPLICATE_NICKNAME);
+            }
+            throw e;
+        }
     }
+
 }
