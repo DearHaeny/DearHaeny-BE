e0cdcc6c2c68020dadecb9b3ec71f0dc4541e104
onlyoneprivate <pllm098@naver.com>
2025-12-18 15:46:20 +0900

    ✨Feat: Gemini 예외 코드 세분화

--- src/main/java/com/dearhaeny/dearhaeny/global/api/code/ErrorStatus.java
+++ src/main/java/com/dearhaeny/dearhaeny/global/api/code/ErrorStatus.java
@@ -14,7 +14,15 @@ public enum ErrorStatus implements BaseErrorCode{
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "E-400-02", "요청 값이 올바르지 않습니다."),
     REQUIRED_FIELD_MISSING(HttpStatus.BAD_REQUEST,"E-400-03", "필수 입력 항목이 누락되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "E-404", "대상을 찾을 수 없습니다."),
-    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "E-409-01", "이미 사용 중인 닉네임입니다.");
+    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "E-409-01", "이미 사용 중인 닉네임입니다."),
+
+    // GEMINI
+    GEMINI_BLOCKED(HttpStatus.BAD_REQUEST, "E-400-03", "Gemini 응답이 정책에 인해 차단되었습니다."),
+    GEMINI_API_KEY_MISSING(HttpStatus.INTERNAL_SERVER_ERROR, "E-500-02", "Gmini API 키가 설정되지 않았습니다."),
+    GEMINI_HTTP_ERROR(HttpStatus.BAD_GATEWAY, "E-502-01", "Gemini HTTP 호출을 실패항였습니다."),
+    GEMINI_INVALID_RESPONSE(HttpStatus.BAD_GATEWAY, "E-502-02", "Gemini 응답 포맷이 올바르지 않습니다."),
+    GEMINI_ERROR_RETURNED(HttpStatus.BAD_GATEWAY, "E-502-03", "Gemini 응답 에러"),
+    GEMINI_EMPTY_TEXT(HttpStatus.BAD_GATEWAY, "E-502-04", "Gemini가 빈 텍스트를 반환했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
